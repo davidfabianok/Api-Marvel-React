e9a3f7bd877009ffe552e982a17099ffd476dd7f
David Fabian <davidfabianok@gmail.com>
2020-03-05 09:50:36 -0300

    seting prettier

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Wrapper from "./wrapper";
-import './style.scss';
+import "./style.scss";
 
 const Modal = ({children, show, onClose}) => {
   return (
     show && (
       <Wrapper className="modal">
-
-
-        
         <div className="modal--content">
           <button className="modal--content__btn-close" onClick={onClose}>
             &times;
